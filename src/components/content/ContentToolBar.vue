733924d7d6007bad8247b898a0d6f0cf47097426
RodionLV <arknim@mail.ru>
2024-03-11 19:34:39 +0900

    edit: edit design

---
+++ src/components/content/ContentToolBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="card q-mt-lg">
+  <div class="card__header">
+    <h6>Загруженные файлы</h6>
+  </div>
+
+  <div class="card__body toolbar toolbar__columns">
+    <div class="toolbar__column">
+      <ui-input-file style="padding: 0.4rem 1.2rem;"/>
+    </div>
+    <div class="toolbar__column">
+      <q-btn dense flat icon="mdi-view-module"/>
+      <q-btn dense flat icon="mdi-view-list"/>
+    </div>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.toolbar{
+  &__columns{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__column{
+    display: flex;
+    gap: 0.2rem;
+  }
+}
+</style>
---
+++ src/components/content/ListContent.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  contents: {type: Array, default: []},
+  selected: {type: Object, default: null}
+})
+
+const emit = defineEmits(['update:selected'])
+
+
+const contents = ref([
+  {name: 'content 1', isSetup: true},
+  {name: 'content 2'},
+  {name: 'content 3'}
+])
+</script>
+
+<template>
+<div class="content">
+  <div class="content__items">
+    <div class="content__item card" v-for="content in contents">
+      <span>{{ content.name }}</span>
+    </div>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.content{
+  &__items{
+    margin-top: 1rem;
+    width: 100%;
+  }
+
+  &__item{
+    padding: 1rem;
+    width: 100%;
+
+    &:not(:first-child){
+      margin-top: 0.4rem;
+    }
+  }
+}
+</style>
