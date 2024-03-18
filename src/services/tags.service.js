4aa025cdd3517ce5c7cb935606dac0ff65c3e537
RodionLV <arknim@mail.ru>
2024-03-18 23:31:26 +0900

    fix: code

--- src/services/tags.service.js
+++ src/services/tags.service.js
@@ -2,7 +2,6 @@ import client from "@/client";
 
 export function getTags({tags = null, types = null}){
   let params = {}
-  console.log(tags)
   if( tags ) params["tags"] = tags.join(",")
   if( types ) params["types"] = types.join(",")
 
