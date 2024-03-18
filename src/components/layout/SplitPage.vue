e2e067c518ed46c2d3025420ffdd006aa7d4fe96
RodionLV <arknim@mail.ru>
2024-03-18 23:30:32 +0900

    add: component SplitPage

---
+++ src/components/layout/SplitPage.vue
@@ -0,0 +1,34 @@
+<script setup>
+// TODO: active
+</script>
+
+<template>
+<div class="split-page">
+  <div class="split-page__body page">
+    <slot></slot>
+  </div>
+  <div class="split-page__sidebar">
+    <slot name="sidebar"></slot>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.split-page{
+  flex: 1 1 0;
+  display: flex;
+  flex-direction: row;
+
+  &__body{
+    flex: 1 1 0;
+  }
+
+  &__sidebar{
+    width: 25vw;
+    
+    &_active{
+      width: 25vw;
+    }
+  }
+}
+</style>
--- src/views/Content.vue
+++ src/views/Content.vue
@@ -2,43 +2,25 @@
 import ListContent from '@/components/content/ListContent.vue'
 import ContentToolBar from '@/components/content/ContentToolBar.vue'
 import ContentSetup from '@/components/content/ContentSetup.vue'
+import SplitPage from '@/components/layout/SplitPage.vue';
 
 import { ref } from 'vue';
-
-let sidebarActive = ref(true)
 
 let selectedContent = ref(null)
 </script>
 
 <template>
-<div class="content">
-  <div class="content__body page">
+<split-page>
+  <template #default>
     <ContentToolBar/>
     <list-content class="q-mt-md" v-model:active="selectedContent"/>
-  </div>
+  </template>
 
-  <div :class="'content__sidebar' + (sidebarActive ? ' content__sidebar_active' : '')">
+  <template #sidebar>
     <ContentSetup :content="selectedContent"/>
-  </div>
-</div>
+  </template>
+</split-page>
 </template>
 
 <style lang="scss" scoped>
-.content{
-  flex: 1 1 0;
-  display: flex;
-  flex-direction: row;
-
-  &__body{
-    flex: 1 1 0;
-  }
-
-  &__sidebar{
-    width: 0;
-    
-    &_active{
-      width: 25vw;
-    }
-  }
-}
 </style>
