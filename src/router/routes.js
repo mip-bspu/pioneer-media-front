0675d1af2a1e9e6a51a407f1e33e24f3b10d414c
RodionLV <arknim@mail.ru>
2024-03-30 18:50:59 +0900

    fix: clear content

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,32 +1,21 @@
 import { computed } from 'vue' 
 
-import Content from '@/views/Content.vue'
 import Device from '@/views/Device.vue'
 import Action from '@/views/Action.vue'
 import Schedule from '@/views/Schedule.vue'
 
 export const routes = [
-  {
-    name: "content",
-    path: "/",
-    component: Content,
-    meta: {
-      name: "Контент",
-      layout: ["sidebar"],
-      icon: "mdi-motion-play"
-    }
-  },
 
-  {
-    name: "devices",
-    path: "/devices",
-    component: Device,
-    meta: {
-      name: "Устройства",
-      layout: ["sidebar"],
-      icon: "mdi-cellphone-link"
-    }
-  },
+  // {
+  //   name: "devices",
+  //   path: "/devices",
+  //   component: Device,
+  //   meta: {
+  //     name: "Устройства",
+  //     layout: ["sidebar"],
+  //     icon: "mdi-cellphone-link"
+  //   }
+  // },
 
   {
     name: "actions",
@@ -39,16 +28,16 @@ export const routes = [
     }
   },
 
-  {
-    name: "schedule",
-    path: "/schedule",
-    component: Schedule,
-    meta: {
-      name: "Расписание",
-      layout: ["sidebar"],
-      icon: "mdi-calendar-range-outline"
-    }
-  },
+  // {
+  //   name: "schedule",
+  //   path: "/schedule",
+  //   component: Schedule,
+  //   meta: {
+  //     name: "Расписание",
+  //     layout: ["sidebar"],
+  //     icon: "mdi-calendar-range-outline"
+  //   }
+  // },
 ]
 
 export const sidebar_routes = computed(()=>{
