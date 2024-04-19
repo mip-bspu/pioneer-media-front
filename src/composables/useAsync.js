d253eda8b0c59ed30a2d69bc0aec8ae76f2f3595
RodionLV <arknim@mail.ru>
2024-04-19 13:04:41 +0900

    add: options for useAsync

--- src/composables/useAsync.js
+++ src/composables/useAsync.js
@@ -1,9 +1,12 @@
 import {reactive} from 'vue';
 import { getMessageError } from '@/utils/format.util.js'
+import { useStore } from '@/store/useStore'
+
+const { store: NotificationStore } = useStore("notification")
 
 const delay = (ms)=>new Promise((res)=>setTimeout(res, ms))
 
-export function useAsync(func){
+export function useAsync(func, opts = { delay: 0, globalError: false, msgSuccess: "" }){
   let state = reactive({
     error: null,
     isError: false,
@@ -15,14 +18,24 @@ export function useAsync(func){
       state.error = null;
       state.isError = false;
       state.isLoading = true;
-      // await delay(4000)
+
+      if( opts["delay"] > 0 )
+        await delay(opts["delay"]);
+
       return await func(...args)
     }catch(e){
       state.isError = true;
       state.error = getMessageError(e);
+
+      if(opts["globalError"])
+        NotificationStore.setMessageError(state.error)
+
       return null;
     }finally{
       state.isLoading = false;
+
+      if(!state.isError && opts["msgSuccess"] != "")
+        NotificationStore.setMessageSuccess(opts["msgSuccess"]);
     }
   }
 
